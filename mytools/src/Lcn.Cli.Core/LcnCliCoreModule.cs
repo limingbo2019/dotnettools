034c62a74a020353c437ae7c174733d8ca60cb1a
limingbo2019 <56576416+limingbo2019@users.noreply.github.com>
2021-09-24 11:54:47 +0800

    初始化工具的模板
    制造工具使用的

---
+++ mytools/src/Lcn.Cli.Core/LcnCliCoreModule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Volo.Abp.Autofac;
+using Volo.Abp.Modularity;
+using Lcn.Cli.Core.Commands;
+using Lcn.Cli.CoreBase;
+
+namespace Lcn.Cli.Core
+{
+
+    [DependsOn(typeof(AbpAutofacModule))]
+    [DependsOn(typeof(LcnCliCoreBaseModule))]
+    public class LcnCliCoreModule : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            Configure<LcnCliCoreBaseOptions>(options => {
+
+                options.Commands["test"] = typeof(MyTestCommand);
+            });
+        }
+    }
+}
---
+++ mytools/src/Lcn.Cli/LcnCliModule.cs
@@ -0,0 +1,24 @@
+﻿using Lcn.Cli.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Autofac;
+using Volo.Abp.Modularity;
+
+namespace Lcn.Cli
+{
+    [DependsOn(typeof(AbpAutofacModule))]
+    [DependsOn(typeof(LcnCliCoreModule))]
+    public class LcnCliModule : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            Configure<CoreBase.LcnCliCoreBaseOptions>(options => {
+
+                options.ToolName = AppDomain.CurrentDomain.FriendlyName;
+            });
+        }
+    }
+}
